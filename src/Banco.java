4fe22bad3707683e5d752ddbad394d1f5ed5154f
Matheus Rodrigues <rodriguesmfc03@gmail.com>
2024-10-04 17:22:59 -0300

    segunda fase

--- src/Banco.java
+++ src/Banco.java
@@ -1,8 +1,31 @@
 public class Banco {
-    public int conta;
-    public int agencia;
-    public double saldo;
+    private int conta;
+    private int agencia;
+    private double saldo;
 
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getConta() {
+        return this.conta;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
 }
 
 
--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,21 @@
 public class Principal {
     public static void main(String[] args) {
         Banco banco1 = new Banco();
-        banco1.agencia = 3268;
-        banco1.conta = 108677;
-        banco1.saldo = 5988.75;
+        banco1.setAgencia(4581);
+        banco1.setConta(834592);
+        banco1.setSaldo(430.00);
 
         Banco banco2 = new Banco();
-        banco2.agencia = 2837;
-        banco2.conta = 586221;
-        banco2.saldo = 10500.25;
+        banco2.setAgencia(2441);
+        banco2.setConta(634093);
+        banco2.setSaldo(2000.00);
 
-        System.out.println("Agência: "+banco1.agencia +"\nConta: "+banco1.conta +"\nSaldo: "+banco1.saldo);
+        System.out.println("Agência: "+banco1.getAgencia() +"\nConta: "+
+                banco1.getConta() +"\nSaldo: "+banco1.getSaldo());
+
         System.out.println("---------------");
-        System.out.println("Agência: "+banco2.agencia +"\nConta: "+banco2.conta +"\nSaldo: "+banco2.saldo);
+
+        System.out.println("Agência: "+banco2.getAgencia() +"\nConta: "+
+                banco2.getConta() +"\nSaldo: "+banco2.getSaldo());
     }
 }
